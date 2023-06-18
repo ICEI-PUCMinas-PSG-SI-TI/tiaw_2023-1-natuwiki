e0d941c1af641f9628e4e25425f13afccf23d576
Davi Martins Freitas de Oliveira Fonseca <davi.fonseca.1405489@sga.pucminas.br>
2023-06-18 12:08:18 -0300

    fazendo carregamento dinâmico de locais para a página principal. Precisei modificar um pouco a base de dados de locais para tal

--- codigo/js/dados.js
+++ codigo/js/dados.js
@@ -91,47 +91,59 @@ dadosLocais = [
     {
         "id": 1,
         "nome": "Cachoeira Alta Vale de Altamira",
-        "descrição": "A Cachoeira Alta é uma bela queda d'água localizada no Vale de Altamira , próximo à cidade de Nova União, em Minas Gerais. Com uma paisagem exuberante e águas cristalinas, é um local perfeito para relaxar e se conectar com a natureza",
-        "endereço": "Vale de Altamira, Nova União, MG",
+        "descricao": "A Cachoeira Alta é uma bela queda d'água localizada no Vale de Altamira , próximo à cidade de Nova União, em Minas Gerais. Com uma paisagem exuberante e águas cristalinas, é um local perfeito para relaxar e se conectar com a natureza",
+        "endereco": "Vale de Altamira, Nova União, MG",
         "como chegar": "Recomenda-se utilizar um veículo particular ou serviço de transporte até Nova União e, em seguida, seguir conforme indicações para o Vale de Altamira.",
         "horário de funcionamento": "Não há informações disponíveis sobre o horário de funcionamento específico. Geralmente, o acesso à cachoeira é permitido durante o dia.",
-        "oferece estadia?": "Não, não há opções de estadia disponíveis no local"
+        "oferece estadia?": "Não, não há opções de estadia disponíveis no local",
+        "imagensprincipal":["/codigo/img/Pagina Principal/CachoeiraAlta.jpeg","/codigo/img/Pagina Principal/CachoeiraAlta (2).jpeg"],
+        "imagensdetalhe":[]
     },
     {
         "id": 2,
         "nome": "Chalé Canto da Siriema",
-        "descrição": "O Chalé Canto da Siriema é um charmoso chalé localizado em Lavras Novas, Minas Gerais. Com uma atmosfera acolhedora e cercada por belas paisagens naturais, é um refúgio perfeito para quem busca paz e tranquilidade.",
-        "endereço": "Lavras Novas, MG",
+        "descricao": "O Chalé Canto da Siriema é um charmoso chalé localizado em Lavras Novas, Minas Gerais. Com uma atmosfera acolhedora e cercada por belas paisagens naturais, é um refúgio perfeito para quem busca paz e tranquilidade.",
+        "endereco": "Lavras Novas, MG",
         "como chegar": "Recomenda-se utilizar um veículo particular ou serviço de transporte Lavras Novas, seguindo até as indicações disponíveis na região.",
         "horário de funcionamento": "Como é um estabelecimento de hospedagem, geralmente está aberto o ano todo, mas é necessário fazer uma reserva antecipada.",
-        "oferece estadia?": "Sim, o Chalé Canto da Siriema oferece estadia para os hóspedes que desejam desfrutar da tranquilidade de Lavras Novas."
+        "oferece estadia?": "Sim, o Chalé Canto da Siriema oferece estadia para os hóspedes que desejam desfrutar da tranquilidade de Lavras Novas.",
+        "imagensprincipal":["/codigo/img/Pagina Principal/Canto da Siriema.jpeg","/codigo/img/Pagina Principal/Canto da Siriema (2).jpeg"],
+        "imagensdetalhe":[]
     }, {
         "id": 3,
         "nome": "Trilha Cachoeira do Lageado",
-        "descrição": "A Trilha Cachoeira do Lageado é um percurso de caminhada que leva até uma bela cachoeira no Vale de Altamira, próximo à cidade de Nova União, em Minas Gerais. A trilha é conhecida por sua beleza natural e paisagens deslumbrantes ao longo do percurso.",
-        "endereço": "Vale de Altamira, Nova União, MG",
+        "descricao": "A Trilha Cachoeira do Lageado é um percurso de caminhada que leva até uma bela cachoeira no Vale de Altamira, próximo à cidade de Nova União, em Minas Gerais. A trilha é conhecida por sua beleza natural e paisagens deslumbrantes ao longo do percurso.",
+        "endereco": "Vale de Altamira, Nova União, MG",
         "como chegar": "Recomenda-se utilizar um veículo particular ou serviço de transporte Nova União e, em seguida até, seguir as indicações para o Vale de Altamira, onde a trilha tem início.",
         "horário de funcionamento": "Não há restrições de horário conhecidos para a trilha. No entanto, é recomendável fazer a caminhada durante o dia e estar atento às condições climáticas.",
         "oferece estadia?": "Não, a trilha não oferece opções de estadia. É uma atividade para ser realizada durante o dia.",
+        "imagensprincipal":["/codigo/img/Pagina Principal/Cachoeira do Lajeado.jpeg","/codigo/img/Pagina Principal/Cachoeira do Lajeado (2).jpeg"],
+        "imagensdetalhe":[]
     }, {
         "id": 4,
         "nome": "Parque da Cachoeira Congonhas",
-        "descrição": "O Parque da Cachoeira Congonhas é um belo parque localizado na cidade de Congonhas, Brasil. É conhecido por suas deslumbrantes cachoeiras, trilhas naturais e paisagens exuberantes.",
-        "endereço": "Rua das Cachoeiras, s/n, Congonhas, Brasil.",
+        "descricao": "O Parque da Cachoeira Congonhas é um belo parque localizado na cidade de Congonhas, Brasil. É conhecido por suas deslumbrantes cachoeiras, trilhas naturais e paisagens exuberantes.",
+        "endereco": "Rua das Cachoeiras, s/n, Congonhas, Brasil.",
         "como chegar": "O Parque da Cachoeira Congonhas está localizado a cerca de 10 km do centro da cidade de Congonhas. Você pode chegar lá de carro, seguindo pela Rua das Cachoeiras até o parque.",
         "horário de funcionamento": "O parque está aberto todos os dias, das 8h às 17h.",
         "oferecer estadia?": "O Parque da Cachoeira Congonhas não oferece estadia. No entanto, existem opções de hospedagem disponíveis nas proximidades.",
+        "imagensprincipal":["/codigo/img/Pagina Principal/Parque da Cachoeira (2).jpeg","/codigo/img/Pagina Principal/Parque da Cachoeira.jpeg", "/codigo/img/Pagina Principal/Parque da Cachoeira (3).jpeg"],
+        "imagensdetalhe":[]
     }, {
         "id": 5,
         "nome": "Cachoeira Véu da Noiva - Serra do Cipó",
-        "descrição": "A Cachoeira Véu da Noiva é uma impressionante queda d'água localizada na Serra do Cipó, em Minas Gerais, Brasil. Com aproximadamente 70 metros de altura, é uma das principais atrações naturais da região.",
-        "endereço": "Estrada da Serra, km 12, Serra do Cipó, Minas Gerais, Brasil.",
+        "descricao": "A Cachoeira Véu da Noiva é uma impressionante queda d'água localizada na Serra do Cipó, em Minas Gerais, Brasil. Com aproximadamente 70 metros de altura, é uma das principais atrações naturais da região.",
+        "endereco": "Estrada da Serra, km 12, Serra do Cipó, Minas Gerais, Brasil.",
         "como chegar": "Para chegar à Cachoeira Véu da Noiva, siga pela Estrada da Serra até o km 12, na Serra do Cipó.",
         "horário de funcionamento": "A cachoeira pode ser visitada diariamente, das 9h às 17h.",
-        "oferecer estadia?": "A Cachoeira Véu da Noiva - Serra do Cipó não oferece estadia, mas há diversas opções de hospedagem disponíveis na região da Serra do Cipó."
+        "oferecer estadia?": "A Cachoeira Véu da Noiva - Serra do Cipó não oferece estadia, mas há diversas opções de hospedagem disponíveis na região da Serra do Cipó.",
+        "imagensprincipal":[],
+        "imagensdetalhe":[]
     }
 ]
 
+if(localStorage.getItem('pessoas') == null)
+    localStorage.setItem('pessoas', JSON.stringify(listaPessoas))
 
-localStorage.setItem('pessoas', JSON.stringify(listaPessoas))
-localStorage.setItem('locais', JSON.stringify(dadosLocais));
+if(localStorage.getItem('locais') == null)
+    localStorage.setItem('locais', JSON.stringify(dadosLocais));
--- codigo/js/principal.js
+++ codigo/js/principal.js
@@ -1,5 +1,66 @@
-var usuario = JSON.parse(localStorage.getItem("usuario"));
+const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-if(usuario == null || usuario.logado == false){
+if (usuario == null || usuario.logado == false) {
     document.location.href = './paginas/login.html';
+}
+
+window.onload = () => {
+    fillPlacesCards();
+}
+
+function shuffleArray(array){
+    return array.map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+}
+
+function fillPlacesCards() {
+    var locais = shuffleArray(JSON.parse(localStorage.getItem('locais')));
+    var placesContainer = document.getElementById('places-container');
+    
+    for (let i = 0; i < 4; i++) {
+        var local = locais[i];
+
+        imagens = ``
+        local.imagensprincipal.forEach(source => {
+            imagens += `
+            <div class="carousel-item active">
+               <img src="${source}" class="d-block w-100 m-auto"
+                alt="${local.nome}">
+            </div>`
+        })
+
+        var cardLocal =
+            `<div class="card col-lg-3 col-md-6 col-xs-12 shadow p-3 mb-5 bg-white rounded border-0">
+                    <h4>${local.nome}</h4>
+                    <div id="carouselExampleIndicators${local.id}" class="carousel slide mt-2">
+                        <div class="carousel-inner">
+                            ${imagens}
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators${local.id}"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators${local.id}"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <div class="rating d-flex mt-2"></div>
+
+                    <p class="card-tag">${local.descricao.slice(0, 150)}...</p>
+                    <i class="fa fa-map-marker d-flex">
+                        <p class="px-2">${local.endereco}</p>
+                    </i>
+                    <div class="leiamais botoes1">
+                        <a href="./paginas/detalhe.html?id=${local.id}">
+                            <p class="mt-2">Leia mais</p>
+                        </a>
+                    </div>
+         </div>`
+
+        placesContainer.innerHTML += cardLocal;
+    }
 }
