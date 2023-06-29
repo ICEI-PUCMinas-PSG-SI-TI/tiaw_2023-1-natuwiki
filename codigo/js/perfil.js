cd09ce008a127227bad3a86f092a95f51e6cd01b
hayanness <hayanne.santos@sga.pucminas.br>
2023-06-29 19:08:04 -0300

    Conectando o local do usuário ao mapa

--- codigo/js/perfil.js
+++ codigo/js/perfil.js
@@ -1,50 +1,22 @@
- //código da Hayanne inicio
- var usuario= JSON.parse(localStorage.getItem("usuario"));
+//código da Hayanne inicio
+var usuario = JSON.parse(localStorage.getItem("usuario"));
 
-if(usuario == null || usuario.logado == false){
+if (usuario == null || usuario.logado == false) {
     document.location.href = './login.html';
 }
 //código da Hayanne fim
 
 window.onload = () => {
-    
     pessoa = load_user_info(page);
-    load_map_info(pessoa);
-
-}
-
-function load_map_info(pessoa){
-    
-    fetch(`https://nominatim.openstreetmap.org/ui/search.html?city=${pessoa.cidade}&limit=1`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-
-    // Creating map options
-    var mapOptions = {
-        center: [-19.9227, -44.0311],
-        zoom: 10
-    }
-
-    // Creating a map object
-    var map = new L.map('my-map', mapOptions);
-
-    // Creating a Layer object
-    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-    // Adding layer to the map
-    map.addLayer(layer);
 }
 
 function load_user_info() {
     var pessoa = JSON.parse(localStorage.getItem("dadosUsuario"));
-    
+
     let enabled_or_disabled = '';
-    if(page == 'perfil'){
+    if (page == 'perfil') {
         enabled_or_disabled = 'disabled'
     }
-    console.log(pessoa.nome)
     user_table_info = `<tbody>
         <tr>
             <td>
